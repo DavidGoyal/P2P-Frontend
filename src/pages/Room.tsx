fa8d737fb0d52e39f1bd941e3ffa5bbe3376cc8f
David <goyaldavid55@gmail.com>
2024-10-25 10:08:51 +0530

    Fixed ontrack maybe

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -6,9 +6,9 @@ const URL = import.meta.env.VITE_BACKEND_URL;
 const iceConfiguration: RTCConfiguration = {
 	iceServers: [
 		{
-			urls: "turn:numb.viagenie.ca",
-			credential: "muazkh",
-			username: "webrtc@live.com",
+			urls: "turn:192.158.29.39:3478?transport=udp",
+			credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+			username: "28224511:1379330808",
 		},
 	],
 };
